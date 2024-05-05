db86a2e954bd9cad2760c42926cf930e1ef3f720
Juan Pablo Marin <juan.marin@studiof.com.co>
2024-05-04 19:01:42 -0500

    feat: funcionalidad servicio para tarea post,put,delete y get

---
+++ backend-todo/backend-todo/Mapping/MappingProfile.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using backend_todo.DTOs.Tarea;
+using backend_todo.Models;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace backend_todo.Mapping
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Tarea, TareaDto>()
+                .ForMember(dest => dest.Estado, opt => opt.MapFrom(src => Enum.GetName(typeof(EstadoTarea), src.Estado)))
+                .ForMember(dest => dest.CategoriaNombre, opt => opt.MapFrom(src => src.Categoria.Nombre));
+
+            CreateMap<CrearTareaDto, Tarea>();
+            CreateMap<ActualizarTareaDto, Tarea>();
+        }
+    }
+}
--- backend-todo/backend-todo/Program.cs
+++ backend-todo/backend-todo/Program.cs
@@ -1,6 +1,28 @@
+using backend_todo.Context;
+using backend_todo.Interface;
+using backend_todo.Mapping;
+using backend_todo.Middleware;
+using backend_todo.Repositories;
+using backend_todo.Services;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddDbContext<TareasDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("TareasDbContext"));
+});
+
+// interface
+builder.Services.AddScoped<ITareaService, TareaService>();
+
+builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+
+//automapper
+builder.Services.AddAutoMapper(typeof(MappingProfile));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
