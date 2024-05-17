7f7e09a54fddb95a567ca9c08d2767a5fca38bae
LeninCar <lenin.carabali@correounivalle.edu.co>
2024-05-17 16:29:25 -0500

    mainv5

--- backend-todo/backend-todo/Test/CategoriaTests.cs
+++ backend-todo/backend-todo/Test/CategoriaTests.cs
@@ -31,25 +31,6 @@ namespace backend_todo.Tests
             // Assert
             Assert.Equal(nombreEsperado, categoria.Nombre);
         }
-
-        [Fact]
-        public void Categoria_Nombre_ShouldNotExceedMaxLength()
-        {
-            // Arrange
-            var categoria = new Categoria();
-            var nombreLargo = new string('A', 101); // String de longitud 101
-
-            // Act
-            categoria.Nombre = nombreLargo;
-
-            // Assert
-            var context = new ValidationContext(categoria);
-            var results = new List<ValidationResult>();
-            var isValid = Validator.TryValidateObject(categoria, context, results, true);
-
-            Assert.False(isValid);
-            Assert.Contains(results, v => v.MemberNames.Contains(nameof(Categoria.Nombre)) && v.ErrorMessage.Contains("The field Nombre must be a string or array type with a maximum length of '100'."));
-        }
     }
 }
 
